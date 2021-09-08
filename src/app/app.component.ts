5896ea26498e1ea316a3f973fb2a84c441c5a94c
amit9812 <amitmaurya0374@gmail.com>
2021-09-08 19:02:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms';
+  answer='';
+  default='pet';
+  genders =['male','female'];
+  showResult:boolean =false;
+  user:any={
+     username:'',
+     email: '',
+     question:'',
+     answer:'', 
+      gender:''
+}
+  @ViewChild('form') loginFormData!: NgForm;
+  defaultQuestion:string ='teacher'
+  suggestUserName() {
+   const suggestedName = 'SuperUser';
+     this.loginFormData.setValue({
+       userData: {
+        username : suggestedName,
+        email: ''
+      },
+      secret: 'pet',questionAnswer: '',
+      gender: 'male'
+    });
+    //  this.loginFormData.form.patchValue({
+    //   userData: {
+    //     username: suggestedName
+    //   }
+    // }); 
+  }
+  
+  
+  /* saveFormData(formData:NgForm){
+console.log(formData.value);
+  } */
+  saveFormData(){
+    console.log(this.loginFormData);
+    this.showResult=true;
+this.user.username = this.loginFormData.value.userData.username;
+this.user.email = this.loginFormData.value.userData.email;
+this.user.question = this.loginFormData.value.secret;
+this.user.answer = this.loginFormData.value.questionAnswer;
+this.user.gender = this.loginFormData.value.gender;
+console.log(this.user);
+  }
+reset(){
+  this.showResult=false;
+}
+  
+  
 }
